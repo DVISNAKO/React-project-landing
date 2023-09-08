7026177f33f58b31da9a32fc7d70822a793f8e4a
Dima <dmitrijs.visnakovs@inbox.lv>
2023-09-08 13:56:02 +0300

    modal registration done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
-import './NavBar.css';
-import Icon from './icons/Icon.png';
-import LogoText from './icons/Nexcent.png';
+import React, { useState } from "react";
+import "./NavBar.css";
+import Icon from "./icons/Icon.png";
+import LogoText from "./icons/Nexcent.png";
+import Modal from "../Modal/Modal";
+
+
 
 const NavBar = () => {
-    return (
-        <nav className='navBar'>
-            <div className='logo'>
-                <img src={Icon} alt='icon'/>
-                <img src={LogoText} alt='logo-text'/>
-                </div>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#features">Features</a></li>
-                <li><a href="#features">Community</a></li>
-                <li><a href="#features">Blog</a></li>
-                <li><a href="#features">Pricing</a></li>
-                <button className='navBtn'>Register Now <span class="material-icons">
-arrow_forward
-</span></button>
-            </ul>
-            
-        </nav>
-    );
+  const [modalActive, setModalActive] = useState(false);
+  return (
+    <nav className="navBar">
+      <div className="logo">
+        <img src={Icon} alt="icon" />
+        <img src={LogoText} alt="logo-text" />
+      </div>
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#features">Features</a>
+        </li>
+        <li>
+          <a href="#features">Community</a>
+        </li>
+        <li>
+          <a href="#features">Blog</a>
+        </li>
+        <li>
+          <a href="#features">Pricing</a>
+        </li>
+        <button className="navBtn" onClick={() => setModalActive(true)}>
+          Register Now
+          <span class="material-icons">arrow_forward</span>
+        </button>
+        <Modal active={modalActive} setActive={setModalActive}/>
+         
+   
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
